e4fd93ace0fae8c0246a77c039b39b1549598291
MyGitPractice <atrimyself@gmail.com>
2023-03-02 07:28:37 +0530

    updated day 4

--- Day - 4/c.Lists.py
+++ Day - 4/c.Lists.py
@@ -1,1 +1,19 @@
-#List in python 
+#List in python Syntax => fruits = [item1,item2]
+
+states_of_India = ['Delhi','TS','AP','UP','MP','J&K']
+print(states_of_India)
+print(states_of_India[0])
+
+#append
+states_of_India.append('Mumbai')
+print(states_of_India)
+
+print(states_of_India[-1])
+
+
+
+#extend
+states_of_India.extend(['Andra','Telangana'])
+print(states_of_India)
+
+print(states_of_India[-1])
